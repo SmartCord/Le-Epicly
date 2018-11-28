a583c813f498a4a9b4d423ec127bb74de1b40cf9
Matt <dankfury9@gmail.com>
2018-11-28 19:43:31 +0800

    Update general.py

--- modules/general.py
+++ modules/general.py
@@ -7,23 +7,6 @@ class GeneralCommands:
         self.bot.remove_command('help')
 
     @commands.command()
-    async def help(self, ctx):
-        try:
-            general = self.bot.get_emoji(515866275503931393)
-            utilities = ":gear:"
-            image_manipulation = ":camera:"
-            e = discord.Embed(title="Bot Help!", description=f"""
-Hey there, Welcome to the interactive help menu. Here you can find pages that contains bot commands.
-You can use the reactions below to navigate through all categories.
-
-{general} - General Commands
-{utilities} - Utiliy Commands
-{image_manipulation} - Image Manipulation Commands
-""", color=utils.color())
-        except Exception as e:
-            await utils.botError(self.bot, ctx, e)
-
-    @commands.command()
     async def ping(self, ctx):
         try:
             pong = round(self.bot.latency*1000)
