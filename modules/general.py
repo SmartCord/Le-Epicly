2c27d7cbd602e806ac1efb1f435ea83b5465b4a8
Matt <dankfury9@gmail.com>
2018-11-29 00:39:12 +0800

    co

--- modules/general.py
+++ modules/general.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 from tools import utils
-import random
+import random, requests
+from io import BytesIO
+from PIL import Image
 
 class GeneralCommands:
     def __init__(self, bot):
@@ -8,11 +10,33 @@ class GeneralCommands:
         self.bot.remove_command('help')
 
     @commands.command()
-    async def ping(self, ctx):
+    async def ping(self, ctx, user: discord.Member = None):
         try:
-            pong = round(self.bot.latency*1000)
-            pong = random.randint(4, 10)
-            await ctx.send(f":ping_pong: Hey, My latency is `{pong}ms`. Now that is epic.")
+            if user is None:
+                pong = round(self.bot.latency*1000)
+                await ctx.send(f":ping_pong: Hey, My latency is `{pong}ms`. Now that is epic.")
+                return
+            page = requests.get(user.avatar_url)
+            page = page.content
+            page = BytesIO(page)
+            avatar = Image.open(page).resize((320, 320)).convert('RGBA')
+            blank = Image.new('RGBA', (256, 256), color=(231, 19, 29))
+            frames = []
+            for i in range(8):
+                base = blank.copy()
+
+                if i == 0:
+                    base.paste(avatar, (-16, -16), avatar)
+                else:
+                    base.paste(avatar, (-32 + randint(-16, 16), -32 + randint(-16, 16)), avatar)
+
+                frames.append(base)
+
+            b = BytesIO()
+            frames[0].save(b, save_all=True, append_images=frames[1:], format='gif', loop=0, duration=20, disposal=2,
+                           optimize=True)
+            b.seek(0)
+            await ctx.send(file=discord.File(b, "ping.png"))
         except Exception as e:
             await utils.botError(self.bot, ctx, e)
 
