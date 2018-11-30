073c61a47504acfd830496697018221f014e48ee
Matt <dankfury9@gmail.com>
2018-11-30 12:13:34 +0800

    Update imports.py

--- imports.py
+++ imports.py
@@ -2,9 +2,8 @@ from discord.ext import commands
 from tools import utils, config
 from tools.bot_tools import db
 from pathlib import Path
-import random, requests, asyncio, discord, time, ast
+import random, requests, asyncio, discord, time, ast, io, traceback, textwrap
 from io import BytesIO
-import io, traceback
 from contextlib import redirect_stdout
 from PIL import Image, ImageFilter, ImageEnhance, ImageDraw, ImageFont
 from tools.utils import color, prefix, usage, error, success, footer, botError
