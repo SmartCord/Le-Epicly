70e8fa049fafa17cb1295b89b1cc81b9ef2424a0
Matt <dankfury9@gmail.com>
2018-11-30 14:09:05 +0800

    Update bot_utils.py

--- tools/bot_utils.py
+++ tools/bot_utils.py
@@ -20,7 +20,7 @@ async def giveAchievement(user, id):
     for x in db.achievements.find({"id":id}):
         reward = f"<:gold:514791023671509003> {x['coins']} Coins\n<:diagay:515536803407593486> {x['diamonds']} Diamonds"
         e = discord.Embed(title=f"Wow New Achievement! Such cool", description=f":clap: Congratulations {user.name} you just obtained the achievement {x['name']}. :clap:\n\nOh and here are your rewards\n{reward}", color=color())
-        e.set_thumbnail(url=gif['clap1'])
+        e.set_thumbnail(url=utils.gif['clap1'])
         footer(user, e)
         await user.send(embed=e)
         db.profiles.update_one({"user_id":user.id}, {'$inc':{'coins':x['coins'], 'diamonds':x['diamonds']}})
